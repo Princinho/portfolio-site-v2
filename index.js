ea4bbdc98a2783c0a200513dc8b796d02e6383da
Princinho <princegnakou@gmail.com>
2022-09-29 10:21:03 +0000

    hide nav when clicked

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 document.getElementById('nav-toggle').addEventListener('click',()=>{
-    document.body.classList.toggle('hidden-nav')
+    document.body.classList.toggle('visible-nav')
 })
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click',hideNav)
+});
+function hideNav(){
+    document.body.classList.remove('visible-nav')
+    console.log('hiding nav')
+}
